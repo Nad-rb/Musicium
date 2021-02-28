c45632b3df44dfa9283b27b99bcb1f488d45b242
Doraemon <71218761+Nad-rb@users.noreply.github.com>
2021-02-28 20:54:51 +0800

    Update uptime.js

--- others/uptime.js
+++ others/uptime.js
@@ -17,7 +17,7 @@ module.exports = {
     let minutes = Math.floor(client.uptime / 60000) % 60;
     let seconds = Math.floor(client.uptime / 1000) % 60;
     //react with approve emoji
-    message.react("769665713124016128");
+    message.react("805819470487617636");
     return message.channel.send(new MessageEmbed().setColor("#c219d8").setTitle(`***__Musicium-Uptime:__***\n\n\`${days}d\` \`${hours}h\` \`${minutes}m\` \`${seconds}s\n\``));
 
 
