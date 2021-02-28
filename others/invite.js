3e7e1832a2d116248df052868350dcc42fdf4c22
Doraemon <71218761+Nad-rb@users.noreply.github.com>
2021-02-28 20:50:31 +0800

    Update invite.js

--- others/invite.js
+++ others/invite.js
@@ -7,16 +7,16 @@ const {
 
 module.exports = {
   name: `invite`,
-  description: `Gives you an invite`,
+  description: `Gives an Dorami bot invite link.`,
   aliases: ["add"],
   cooldown: 3,
   edesc: "Type this command to get an invite link for the Bot, thanks for every Invite",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("769665713124016128");
+    message.react("805819470487617636");
     //send the invite embed
-    message.reply(new MessageEmbed().setColor("#c219d8").setTitle(":heart: Please Invite me: ").setDescription("https://discord.com/oauth2/authorize?client_id=769642999227351070&permissions=0&scope=bot")
-    .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/avatars/769642999227351070/f1b78891507308fb76c0a66b56f4bcd6.webp?size=512"));
+    message.reply(new MessageEmbed().setColor("#c219d8").setTitle(":heart: Please Invite me: ").setDescription("https://discord.com/oauth2/authorize?client_id=805745585097277440&permissions=37080128&scope=bot")
+    .setFooter("And enjoy listening to music!", "https://cdn.discordapp.com/attachments/805764806224510977/815565682347212830/PicsArt_02-28-08.46.34.jpg"));
 
   }
 }
