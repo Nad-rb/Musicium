46285a983bde95005e0b16c68e5b2e50d46459fc
Doraemon <71218761+Nad-rb@users.noreply.github.com>
2021-02-28 20:37:36 +0800

    Update botlist.js

--- others/botlist.js
+++ others/botlist.js
@@ -13,14 +13,12 @@ module.exports = {
   edesc: "Type this command to view all Bot list Server where the bot is on. Please vote there UwU",
   execute(message, args, client) {
     //react with approve emoji
-    message.react("769665713124016128");
+    message.react("805819470487617636");
     //send the botlist embed
     message.reply(new MessageEmbed().setColor("#c219d8")
     .setTitle("Here is a list for all Bot-Lists this Bot is on!")
-    .addField("Matrixbots","https://www.matrixbots.xyz/bots/769642999227351070/")
-    .addField("top.gg", "https://top.gg/bot/769642999227351070")
-    .addField("bots.ondiscord","https://bots.ondiscord.xyz/bots/769642999227351070")
-    .addField("discordbotlist","https://discordbotlist.com/bots/musicium")
+    .addField("Dorami","https://www.discord.ly/dorami")
+    .addField("potatobot", "https://discord.ly/potatobot-1799")
     );
 
   }
